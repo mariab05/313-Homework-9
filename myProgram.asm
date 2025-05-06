8f6d3a655839b21b35035b11e7cb23f4f1c6ee17
mariab05 <143639780+mariab05@users.noreply.github.com>
2025-05-05 20:59:50 -0400

    Update myProgram.asm
    added compile comment changes

--- myProgram.asm
+++ myProgram.asm
@@ -1,7 +1,7 @@
-; Hello World Program - asmtutor.com
-; Compile with: nasm -f elf helloworld.asm
-; Link with (64 bit systems require elf_i386 option): ld -m elf_i386 helloworld.o -o helloworld
-; Run with: ./helloworld
+; Hello World Program - Maria Bryant
+; Compile with: nasm -f elf myProgram.asm
+; Link with (64 bit systems require elf_i386 option): ld -m elf_i386 myProgram.o -o myProgram
+; Run with: ./myProgram
  
 SECTION .data
 msg     db      'Hello World!', 0Ah
